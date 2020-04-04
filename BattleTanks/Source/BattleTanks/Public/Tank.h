7dea81ceeb0b6f00830631d2ae11e0d03ae2553b
Kenny <km.kratzer@gmail.com>
2020-04-04 16:53:05 -0400

    Tank Aiming Component created and reports hit location

--- BattleTanks/Source/BattleTanks/Public/Tank.h
+++ BattleTanks/Source/BattleTanks/Public/Tank.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "TankAimingComponent.h"
+// Always do this last
 #include "Tank.generated.h"
 
 UCLASS()
@@ -18,6 +20,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 public:	
 	// Called every frame
